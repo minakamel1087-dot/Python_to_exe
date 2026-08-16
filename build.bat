9b0070e67ffaba5f5fda5546c9ebd2ecc5e3390b
minakamel1087-dot <mina.kamel1087@gmail.com>
2026-08-16 22:56:42 +0400

    Delete build.bat

--- build.bat
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-REM One-file build. --noconsole hides the terminal; drop it while debugging.
-pyinstaller --noconfirm --onefile --noconsole --name "WIR Tools" ^
-    --hidden-import win32timezone ^
-    main.py
